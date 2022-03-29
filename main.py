879a5cf9efa686bf233868c6911c294038d8b886
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2022-03-29 21:53:25 +0800

    Improve code

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-SHUFFLE_BUFFER_SIZE = 1024
-BATCH_SIZE = 32
+SHUFFLE_BUFFER_SIZE = 65536
 NUM_EPOCHS = 4
+MAX_BATCH_SIZE = 1024
+DATASET_NUM_BUCKETS = 18
+DATASET_BUCKET_BOUNDARIES = [2 ** i for i in range(DATASET_NUM_BUCKETS)]
+DATASET_BUCKET_BATCH_SIZES = [min(2 ** i, MAX_BATCH_SIZE) for i in range(DATASET_NUM_BUCKETS)][::-1] + [1]
 
-EMBEDDING_DIM = 16
-NUM_LSTM_UNITS = 32
+EMBEDDING_DIM = 32
+NUM_LSTM_UNITS = 128
 NUM_HIDDEN_NODES = 512
 NUM_OUTPUT_NODES = 4
 
@@ -18,22 +21,29 @@ def preprocess_data(text_vectorisation_layer, raw_text, raw_label):
     label = tf.one_hot(raw_label - 1, 4)
     return text, label
 
-def get_train_data():
+def load_train_data():
     train_df = pd.read_csv('./raw_data/fulltrain.csv', names=['label', 'text'])
-    train_df = train_df.sample(frac=1) # shuffle
-    train_label_df = train_df['label']
-    train_text_df = train_df['text']
-    train_label_arr = np.array(train_label_df)
-    train_text_arr = np.array(train_text_df)
-    return train_label_arr, train_text_arr
+    return train_df
 
-def create_train_dataset(train_label_arr, train_text_arr, preprocess_fn):
-    raw_train_ds = tf.data.Dataset.from_tensor_slices((train_text_arr, train_label_arr))
-    train_ds = raw_train_ds.map(preprocess_fn)
-    train_ds = train_ds.shuffle(SHUFFLE_BUFFER_SIZE)
-    train_ds = train_ds.padded_batch(BATCH_SIZE)
-    train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
-    return train_ds
+def get_data_arrs(df):
+    text_df = df['text']
+    label_df = df['label']
+    text_arr = np.array(text_df)
+    label_arr = np.array(label_df)
+    return text_arr, label_arr
+
+def create_dataset(raw_text_arr, raw_label_arr, preprocess_fn, needs_shuffle=True):
+    ds = tf.data.Dataset.from_tensor_slices((raw_text_arr, raw_label_arr))
+    ds = ds.map(preprocess_fn)
+    if needs_shuffle:
+        ds = ds.shuffle(SHUFFLE_BUFFER_SIZE)
+    ds = ds.bucket_by_sequence_length(
+        element_length_func=lambda text, label: tf.shape(text)[0],
+        bucket_boundaries=DATASET_BUCKET_BOUNDARIES,
+        bucket_batch_sizes=DATASET_BUCKET_BATCH_SIZES
+    )
+    ds = ds.prefetch(tf.data.AUTOTUNE)
+    return ds
 
 def create_model(vocab_size):
     model = tf.keras.Sequential([
@@ -70,7 +80,7 @@ def create_model(vocab_size):
     initial_learning_rate = 1e-2
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate,
-        decay_steps=1e3,
+        decay_steps=7e2,
         decay_rate=0.1
     )
     model.compile(
@@ -96,20 +106,9 @@ def train(model, train_ds, save_ckpt_path=None):
     )
     return history
 
-def get_test_data():
+def load_test_data():
     test_df = pd.read_csv('./raw_data/balancedtest.csv', names=['label', 'text'])
-    test_label_df = test_df['label']
-    test_text_df = test_df['text']
-    test_label_arr = np.array(test_label_df)
-    test_text_arr = np.array(test_text_df)
-    return test_label_arr, test_text_arr
-
-def create_test_dataset(preprocess_fn):
-    test_label_arr, test_text_arr = get_test_data()
-    raw_test_ds = tf.data.Dataset.from_tensor_slices((test_text_arr, test_label_arr))
-    test_ds = raw_test_ds.map(preprocess_fn)
-    test_ds = test_ds.padded_batch(BATCH_SIZE)
-    return test_ds
+    return test_df
 
 def evaluate(model, test_ds, load_ckpt_path=None):
     if load_ckpt_path:
@@ -118,17 +117,20 @@ def evaluate(model, test_ds, load_ckpt_path=None):
     return loss, accuracy
 
 def main(args):
-    train_label_arr, train_text_arr = get_train_data()
+    train_df = load_train_data()
+    train_text_arr, train_label_arr = get_data_arrs(train_df)
     tvl = tf.keras.layers.TextVectorization()
     tvl.adapt(train_text_arr)
     preprocess_data_using_tvl = lambda raw_text, raw_label: preprocess_data(tvl, raw_text, raw_label)
     model = create_model(tvl.vocabulary_size())
     model.summary()
     if args.task in {'train', 'full'}:
-        train_ds = create_train_dataset(train_label_arr, train_text_arr, preprocess_data_using_tvl)
+        train_ds = create_dataset(train_text_arr, train_label_arr, preprocess_data_using_tvl)
         history = train(model, train_ds, save_ckpt_path=args.save_ckpt_path)
     if args.task in {'test', 'full'}:
-        test_ds = create_test_dataset(preprocess_data_using_tvl)
+        test_df = load_test_data()
+        test_text_arr, test_label_arr = get_data_arrs(test_df)
+        test_ds = create_dataset(test_text_arr, test_label_arr, preprocess_data_using_tvl)
         loss, accuracy = evaluate(model, test_ds, load_ckpt_path=args.load_ckpt_path)
         print('Loss:', loss)
         print('Accuracy:', accuracy)
