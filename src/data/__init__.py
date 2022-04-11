1094f908a8d4a6e297706a6e90920a5d00dae470
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2022-04-11 23:58:22 +0800

    Convert Isaac's BuzzFeed data code

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,8 +2,10 @@ from data.dataset import Dataset
 from data.dataset_using_dataframe import DatasetUsingDataframe
 from data.bupt_gamma import BuptGamma
 from data.bupt_gamma_with_validation import BuptGammaWithValidation
+from data.buzzfeed import BuzzFeed
 
 datasets = {
     'bupt_gamma': BuptGamma,
-    'bupt_gamma_with_validation': BuptGammaWithValidation
+    'bupt_gamma_with_validation': BuptGammaWithValidation,
+    'buzzfeed': BuzzFeed
 }
---
+++ src/data/buzzfeed.py
@@ -0,0 +1,44 @@
+import os
+from typing import Tuple
+from typing import Union
+
+import numpy as np
+import pandas as pd
+
+from data import DatasetUsingDataframe
+
+DIR = os.path.dirname(__file__)
+
+TEST_TRAINVAL_SPLIT_FRACTION = 0.2
+VAL_TRAIN_SPLIT_FRACTION = 0.2
+
+class BuzzFeed(DatasetUsingDataframe):
+    def __init__(self) -> None:
+        super().__init__()
+        buzzfeedfake = pd.read_csv(os.path.join(DIR, '..', '..', 'Data', 'BuzzFeed_fake_news_content.csv'))
+        buzzfeedfake['label'] = 1
+        buzzfeedfake = buzzfeedfake.drop(columns=['title', 'id', 'url', 'top_img', 'authors', 'source', 'publish_date', 'movies', 'images', 'canonical_link', 'meta_data'])
+        buzzfeedreal = pd.read_csv(os.path.join(DIR, '..', '..', 'Data', 'BuzzFeed_fake_news_content.csv'))
+        buzzfeedreal['label'] = 0
+        buzzfeedreal = buzzfeedreal.drop(columns=['title', 'id', 'url', 'top_img', 'authors', 'source', 'publish_date', 'movies', 'images', 'canonical_link', 'meta_data'])
+        df = pd.concat([buzzfeedfake, buzzfeedreal])
+        test_df, valtrain_df = self._split_stratified(df, TEST_TRAINVAL_SPLIT_FRACTION, 'label')
+        val_df, train_df = self._split_stratified(valtrain_df, VAL_TRAIN_SPLIT_FRACTION, 'label')
+        self._set_training_dataframe(train_df)
+        self._set_validation_dataframe(val_df)
+        self._set_test_dataframe(test_df)
+
+    def get_training_data(self) -> Tuple[np.ndarray, np.ndarray]:
+        train_df = self._get_training_dataframe()
+        text_arr, label_arr = self._get_data_arrays(train_df, ['text', 'label'])
+        return text_arr, label_arr
+
+    def get_validation_data(self) -> Union[Tuple[np.ndarray, np.ndarray], None]:
+        val_df = self._get_validation_dataframe()
+        text_arr, label_arr = self._get_data_arrays(val_df, ['text', 'label'])
+        return text_arr, label_arr
+
+    def get_test_data(self) -> Tuple[np.ndarray, np.ndarray]:
+        test_df = self._get_test_dataframe()
+        text_arr, label_arr = self._get_data_arrays(test_df, ['text', 'label'])
+        return text_arr, label_arr
